677d215e129331a32e764a73cd805e09adc65eff
lvke <lvke>
2022-08-21 12:37:31 +0800

    123

---
+++ DirectXWithQt/SceneShader.cpp
@@ -0,0 +1,42 @@
+#include "SceneShader.h"
+
+std::vector<std::vector<std::wstring>> SceneShader::shaderPath =
+{
+	{
+		L"Shader\\Chapter 1\\VS.hlsl",
+		L"Shader\\Chapter 1\\PS.hlsl"
+	},
+	{
+		L"Shader\\Chapter 2\\VS.hlsl",
+		L"Shader\\Chapter 2\\PS.hlsl"
+	},
+	{
+		L"Shader\\Chapter 3\\VS.hlsl",
+		L"Shader\\Chapter 3\\PS.hlsl"
+	},
+	{
+		L"Shader\\Chapter 4\\VS.hlsl",
+		L"Shader\\Chapter 4\\PS.hlsl"
+	},
+	{
+		L"Shader\\Chapter 5\\VS.hlsl",
+		L"Shader\\Chapter 5\\PS.hlsl"
+	},
+	{
+		L"Shader\\Chapter 6\\VS.hlsl",
+		L"Shader\\Chapter 6\\PS.hlsl"
+	},
+	{
+		L"Shader\\Chapter 7\\VS.hlsl",
+
+		L"Shader\\Chapter 7\\PS.hlsl"
+	},
+	{
+		L"Shader\\Chapter 8\\VS.hlsl",
+		L"Shader\\Chapter 8\\PS.hlsl"
+	},
+	{
+		L"Shader\\Chapter 9\\VS.hlsl",
+		L"Shader\\Chapter 9\\PS.hlsl"
+	}
+};
---
+++ DirectXWithQt/SceneShader.h
@@ -0,0 +1,10 @@
+#pragma
+
+#include <vector>
+#include <string>
+
+class SceneShader
+{
+public:
+	static std::vector<std::vector<std::wstring>> shaderPath;
+};
