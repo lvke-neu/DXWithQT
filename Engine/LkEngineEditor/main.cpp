fd74e3b485bae7d5cb67ae8b336f167df0ef3561
lvke <lvke>
2022-09-18 00:46:16 +0800

    调整了下编译顺序 （在生成dll文件（动态库文件）时，如果不使用_declspec(dllexport)那么就只有dll文件，在这种情况下就无法使用#pragma comment来隐式装载动态库（因为需要lib文件），只能使用LoadLibrary来显式装载动态库（使用Loadlibrary只需要dll文件）
    如果使用了_declspec(dllexport)那么就既有dll文件，也有lib文件
    
    还有一点需要注意的是，如果在源文件（.c文件）中函数的定义没有_declspec(dllexport)
    
    ，但是在头文件中函数的声明使用了_declspec(dllexport)此时编译产生的文件只有dll文件，如果改成源文件中有_declspec(dllexport)，头文件中没有_declspec(dllexport)，那么编译产生的文件既有dll文件也有lib文件
    
    （导出dll文件时最好还是在源文件和头文件中都加上_declspec(dllexport)）
    ————————————————
    版权声明：本文为CSDN博主「Love coldplay」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
    原文链接：https://blog.csdn.net/weixin_40933653/article/details/123946449）

--- Engine/LkEngineEditor/main.cpp
+++ Engine/LkEngineEditor/main.cpp
@@ -1,8 +1,12 @@
 #include "LkEngineEditor.h"
 #include <QtWidgets/QApplication>
+#include "../LkEngineRuntime/Core/core.h"
 
 int main(int argc, char *argv[])
 {
+	A aa;
+	aa.test();
+
     if (QT_VERSION >= QT_VERSION_CHECK(5, 6, 0))
         QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
