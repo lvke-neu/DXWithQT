6196110abea5b70006fa14f05e52e5f5f681ebfe
lvke <lvke>
2022-09-17 22:41:44 +0800

    编译生成路径

--- Engine/LkEngineEditor/mainwindow.h
+++ Engine/LkEngineEditor/mainwindow.h
@@ -1,16 +1,9 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
-#include <QListWidgetItem>
-#include <QLabel>
-#include <QDialog>
-#include <QPushbutton>
-#include <QTextEdit>
+
 #include "ui_mainwindow.h"
-#include "d3d11renderwidget.h"
-#include "ChapterDockWidgetInterface.h"
-#include "Service.h"
+
 
 namespace Ui {
 class MainWindow;
@@ -20,24 +13,15 @@ class MainWindow : public QMainWindow
 {
 	Q_OBJECT
 
-	//friend class IChapterDockWidget;
+
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 
-
-public slots:
-    void changeChapter(QListWidgetItem* item);
-
-public:
-	void setIntroduction(QListWidgetItem* item);
 private:
     Ui::MainWindow *ui;
-	D3d11RenderWidget *m_RenderWidget{ nullptr };
-	QLabel* m_labelIntroduction{ nullptr };
 
-	std::map<std::string, IChapterDockWidget*> m_chapterDockWidgets;
 
 };
 
