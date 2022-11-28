170f38007270203f0efdca3566de742629130b7b
lvke <lvke>
2022-11-28 21:46:19 +0800

    Twinkle 初始化提交

--- Engine/LkEngineRuntime/Scene/Shader/ParticleComponent/ParticleComponentPS.hlsl
+++
@@ -1,9 +0,0 @@
-#include "ParticleComponent.hlsli"
-
-
-
-float4 PS(VertexOut pIn) : SV_Target
-{
-	//return float4(1.0f, 0.0f, 0.0f, 1.0f);
-	return texColor;
-}
---
+++ Engine/Runtime/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello Twinkle" << std::endl;
+	getchar();
+	return 0;
+}
