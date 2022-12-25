deea5b58d510a8c651a9e2c4956ce5943b28cb34
lvke <lvke>
2022-12-25 20:49:53 +0800

    描边效果

--- Engine/Runtime/Scene/SceneManager.cpp
+++ Engine/Runtime/Scene/SceneManager.cpp
@@ -9,14 +9,19 @@
 
 namespace Twinkle
 {
-	IBindable* dssNoDepthTest;
+	IBindable* dssWrite;
+	IBindable* dssMask;
+	IGameObject* outlineCube{ nullptr };
+	IGameObject* outlineCylinder{ nullptr };
 	SceneManager::SceneManager()
 	{
 		Singleton<PerspectiveCamera>::GetInstance().SetPosition(0.0f, 0.0f, -10.0f);
 		Singleton<CameraController>::GetInstance();
 
 		IGameObject* cube = new BasicGeometryGameObject(Cube);
+		outlineCube = new BasicGeometryGameObject(Cube);
 		IGameObject* cylinder = new BasicGeometryGameObject(Cylinder);
+		outlineCylinder = new BasicGeometryGameObject(Cylinder);
 		IGameObject* plane = new BasicGeometryGameObject(Plane);
 		IGameObject* m_light = new BasicGeometryGameObject(Sphere);
 		m_light->getTransformComponent()->setScale(0.5f, 0.5f, 0.5f);
@@ -25,12 +30,13 @@ namespace Twinkle
 
 		m_sceneGameObjects.push_back(cube);
 		m_sceneGameObjects.push_back(cylinder);
-		m_sceneGameObjects.push_back(plane);
+		//m_sceneGameObjects.push_back(plane);
 		m_sceneGameObjects.push_back(m_light);
 
 		if (cylinder->getTransformComponent())
 		{
 			cylinder->getTransformComponent()->setPosition(0.0f, 0.0f, 5.0f);
+			outlineCylinder->getTransformComponent()->setPosition(0.0f, 0.0f, 5.0f);
 		}
 		
 		if (plane->getTransformComponent())
@@ -45,6 +51,9 @@ namespace Twinkle
 
 
 		Singleton<ImGuiManager>::GetInstance().setSceneGameObjects(m_sceneGameObjects);
+
+		dssWrite = Singleton<RenderSystem>::GetInstance().CreateDepthStencilState(DSSWrite, 0xFF);
+		dssMask = Singleton<RenderSystem>::GetInstance().CreateDepthStencilState(DSSMask, 0xFF);
 	}
 
 	SceneManager::~SceneManager()
@@ -58,14 +67,27 @@ namespace Twinkle
 
 	void SceneManager::Tick(float deltaTime)
 	{
-		static float y = 0.0f;
-		y += deltaTime;
-		m_sceneGameObjects[0]->getTransformComponent()->setRotation(0.0f, y, 0.0f);
+		//static float y = 0.0f;
+		//y += deltaTime;
+		//m_sceneGameObjects[0]->getTransformComponent()->setRotation(0.0f, y, 0.0f);
+
+		Singleton<RenderSystem>::GetInstance().SetDepthStencilState(dssMask);
+		outlineCube->getTransformComponent()->setScale(1.05, 1.05, 1.05);
+		outlineCube->getMeshComponent()->setPsShader("\\builtin\\BinShader\\LightSpherePixelShader.cso");
+		outlineCube->tick(deltaTime);
+		outlineCylinder->getTransformComponent()->setScale(1.05, 1.05, 1.05);
+		outlineCylinder->getMeshComponent()->setPsShader("\\builtin\\BinShader\\LightSpherePixelShader.cso");
+		outlineCylinder->tick(deltaTime);
+		
 
 
+		Singleton<RenderSystem>::GetInstance().SetDepthStencilState(dssWrite);
 		for (auto& go : m_sceneGameObjects)
 		{
 			go->tick(deltaTime);
 		}
+
+	
+
 	}
 }
--- Engine/Runtime/Shader/LightSpherePixelShader.hlsl
+++ Engine/Runtime/Shader/LightSpherePixelShader.hlsl
@@ -2,5 +2,6 @@
 
 float4 main(VsOut pIn) : SV_Target
 {
-	return float4(1.0f, 1.0f, 1.0f, 1.0f);
+	//return float4(1.0f, 1.0f, 1.0f, 1.0f);
+	return float4(1.0f,0.4f,0.4f,1.0f);
 }
